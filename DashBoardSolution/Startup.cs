41f9ae839be6c0cd139288b243b878fe16053993
JoshiVi <JoshiVi>
2016-11-24 15:28:33 +0530

    Removed (unloaded) un-used files.

--- DashBoardSolution/Startup.cs
+++ DashBoardSolution/Startup.cs
@@ -8,7 +8,7 @@ namespace DashBoardSolution
     { 
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            //ConfigureAuth(app);
         }
     }
 }
